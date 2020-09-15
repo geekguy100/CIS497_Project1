0d9929332228c30ff05ca1320fc23774034f6e83
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-09-15 13:04:10 -0500

    Created basic room and player

---
+++ Project1/Assets/Scripts/TPC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPC : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move forward
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rotation
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
