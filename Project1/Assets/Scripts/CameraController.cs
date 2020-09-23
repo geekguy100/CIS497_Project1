be8e2e8130d3de026cde2c736e5d4616d484e7f0
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2020-09-23 14:39:22 -0500

    Finsihed Player controller and camrea scripts

--- Project1/Assets/Scripts/CameraController.cs
+++ Project1/Assets/Scripts/CameraController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject Player;
+    private float mouseX;
+    private float mouseY;
+    private float xRotation = 0f;
+
+    public float sensitivity = 100f;
+    public Transform playerBody;
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Project1/Assets/Scripts/PlayerController.cs
+++ Project1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed;
-    public float moveHorizontal;
-    public float moveVertical;
 
-    void Start()
-    {
-        
-    }
+    //private variables
+    private float moveHorizontal;
+    private float moveVertical;
+    public Vector3 velocity;
+    private bool isGrounded;
+
+    //public variables
+    public float gravity = -9.81f;
+    public float speed = 12f;
+    public float groundDistance = .4f;
+    public float jumpHeight = 3f;
+    public CharacterController Controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
 
     // Update is called once per frame
     void Update()
@@ -22,7 +29,33 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
+        //Checks if the player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //resets the Up/down velocity of the player if the player is on the ground
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Sets the inputs for movement
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
+
+        //allows the player to move around
+        Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
+        Controller.Move(move * speed * Time.deltaTime);
+
+        //Checks if the player is on the gound and if the player is it will allow the player to jump
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Pulls the player back down to the ground using gravity
+        velocity.y += gravity * Time.deltaTime;
+        Controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
